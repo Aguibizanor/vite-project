de30e1ee376b5f7a2edee86804004a5acbc860c8
JoaoMotaSilva10 <joaomotasilva10@outlook.com>
2024-09-12 09:48:23 -0300

    final

--- src/App.jsx
+++ src/App.jsx
@@ -11,41 +11,34 @@ function App() {
                 <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-4xl">
                     <h2 className="text-center text-2xl font-bold text-gradient mb-6 titulo ">CRIAR CONTA</h2>
                     <div className="grid grid-cols-2 gap-4">
-                        <div>
-                            <label className="block mb-2">Nome:</label>
-                            <input type="text" className="w-full p-2 border rounded" />
+                        <div className="input-single">
+                            <input type="text" className="w-full p-2 border rounded input" placeholder="Nome:" />
                         </div>
-                        <div>
-                            <label className="block mb-2">Sobrenome:</label>
-                            <input type="text" className="w-full p-2 border rounded" />
+                        <div className="input-single">
+                            <input type="text" className="w-full p-2 border rounded input" placeholder="Sobrenome:"/>
                         </div>
-                        <div>
-                            <label className="block mb-2">CPF:</label>
-                            <input type="text" className="w-full p-2 border rounded" />
+                        <div className="input-single">
+                            <input type="text" className="w-full p-2 border rounded input" placeholder="CPF:" />
                         </div>
-                        <div>
-                            <label className="block mb-2">Data de nascimento:</label>
-                            <input type="date" className="w-full p-2 border rounded" />
+                        <div className="input-single">
+                            <input type="text" className="w-full p-2 border rounded input" placeholder="Data de nascimento:" />
                         </div>
-                        <div>
-                            <label className="block mb-2">Email:</label>
-                            <input type="email" className="w-full p-2 border rounded" />
+                        <div className="input-single">
+                            <input type="text" className="w-full p-2 border rounded input" placeholder="Email:" />
                         </div>
-                        <div>
-                            <label className="block mb-2">Telefone:</label>
-                            <input type="tel" className="w-full p-2 border rounded" />
+                        <div className="input-single">
+                            <input type="text" className="w-full p-2 border rounded input" placeholder="Telefone:" />
                         </div>
-                        <div>
-                            <label className="block mb-2">Senha:</label>
-                            <input type="password" className="w-full p-2 border rounded" />
+                        <div className="input-single">
+                            <input type="text" className="w-full p-2 border rounded input" placeholder="Senha:" />
                         </div>
-                        <div>
-                            <label className="block mb-2">Confirmar Senha:</label>
-                            <input type="password" className="w-full p-2 border rounded" />
+                        <div className="input-single">
+                            <input type="text" className="w-full p-2 border rounded input" placeholder="Confirmar senha:" />
                         </div>
+                        
                     </div>
                     <div className="flex justify-center mt-6">
-                        <button className="bg-gradient-to-r from-blue-500 to-purple-500 text-white py-2 px-4 rounded">CADASTRE-SE</button>
+                        <button className="bg-gradient-to-r from-blue-500 to-purple-500 text-white py-2 px-4 rounded button-color">CADASTRE-SE</button>
                     </div>
                     <div className="text-center mt-4">
                         <span>Já tem uma conta? Faça login: <a href="#" className="text-blue-500">Login</a></span>
